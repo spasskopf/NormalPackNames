6f28be6f6fecc716ad0961878a351a942f1656db
spasskopf <78727783+spasskopf@users.noreply.github.com>
2021-08-14 15:39:47 +0200

    Fix potential problems: Filename only contains filtered characters ("!!!!  ") New name already exists (time + Random number) Python is too fast and the nanosecond timestamp is identical (Random number)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import sys
 import os
+import time
+import random
 
 searchExpression = r'^[! ]+'
 
@@ -13,9 +15,20 @@ def clean_directory(directory):
     for file in os.listdir(directory):
         if re.search(searchExpression, file) is not None:
             name = re.sub(searchExpression, "", file)
+
+            # Name only consists of filtered symbols
+            if len(name) == 0:
+                print(file + " only consists of filtered symbols! Using fallback name")
+                name = "fallback_" + str(time.time_ns() + random.randint(0, 10000))
+
+            if os.path.exists(directory + os.sep + name):
+                print(file + " already exists! Appending current time")
+                name += str(time.time_ns() - random.randint(0, 10000))
+
             os.rename(directory + os.sep + file, directory + os.sep + name)
-            print("Cleaning '" + file + "' -> '" + name + "'")
+            print("Renaming '" + file + "' -> '" + name + "'")
 
 
 for arg in sys.argv:
     clean_directory(arg)
+print("Finished")
